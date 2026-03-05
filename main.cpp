58b61eb81a60790ddc04f944752db4835f96d632
grisha-git <donkeevgrisa@gmail.com>
2026-03-05 13:16:59 +0300

    New head function

--- main.cpp
+++ main.cpp
@@ -7,3 +7,13 @@ struct BiList
   BiList< T >* next;
   BiList< T >* prev;
 };
+
+template< class T >
+BiList< T >* newHead(BiList< T >* head, const T& target)
+{
+  BiList< T >* newNode = new BiList< T >{target, head->next, head->prev};
+  head->next->prev = newNode;
+  head->prev->next = newNode;
+  delete head;
+  return newNode;
+}
